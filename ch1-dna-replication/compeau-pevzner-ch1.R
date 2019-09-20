5b66b9e1bb68f491f5ca8be96bdf6f7af6993762
Shannon Largman <shannon.largman@gmail.com>
2019-09-20 14:28:02 -0700

    Add PatternToNumber and NumberToPattern
    PatternToNumber and NumberToPattern are preliminary functions to solve
    the pg. 39 Charging Station (The frequency array) for a faster version
    of FrequentWords to count the most frequent k-mer in a character string.
    This commit solves the Charging Station on pg. 41 (Converting patterns
    to numbers and vice-versa) which lays the groundwork for "The frequency
    array".
    
    PatternToNumber converts a nucleotide sequence into an integer giving
    its lexicographic order (starting at index 0). It uses the function
    SymbolToNumber and solves Coding Challenge 1L.
    
    NumberToPattern converts an inteer into a nucleotide sequence of a given
    length k with the corresponding lexicographic index (again starting at
    0). It is the inverse to PatternToNumber. It uses the function
    NumberToSymbol and solves Coding Challenge 1M.

--- ch1-dna-replication/compeau-pevzner-ch1.R
+++ ch1-dna-replication/compeau-pevzner-ch1.R
@@ -5,7 +5,7 @@
 #' @param text character string to be scanned for occurrences of the string \code{pattern}
 #' @param pattern character string to be matched in the given character string
 #' @examples
-#' PatternCount('CGATATATCCATAG', 'ATA')
+#' PatternCount("CGATATATCCATAG", "ATA")
 PatternCount <- function(text, pattern){
 	count <- 0
 	for(i in 1:(nchar(text) - nchar(pattern) + 1)){
@@ -24,8 +24,8 @@ PatternCount <- function(text, pattern){
 #' @param k integer which gives the length of \emph{k}-mer
 #' @return character vector of all the most frequent \emph{k}-mers in \code{text}
 #' @examples
-#' FrequentWords('ACAACTATGCATACTATCGGGAACTATCCT', 5)
-#' FrequentWords('CGATATATCCATAG', 3)
+#' FrequentWords("ACAACTATGCATACTATCGGGAACTATCCT", 5)
+#' FrequentWords("CGATATATCCATAG", 3)
 FrequentWords <- function(text, k){
 	count <- vector()
 	for (i in 1:(nchar(text) - k + 1)){
@@ -38,4 +38,73 @@ FrequentWords <- function(text, k){
 	# remove duplicates
 	frequent_patterns <- sort(unique(frequent_patterns))
 	return(frequent_patterns)
+}
+
+#' Transforms nucleotide symbols into lexicographic index
+#' 
+#' \code{SymbolToNumber} transforms the symbols A, C, G, and T into the respective integers 0, 1, 2, and 3. This corresponds to the indexing starting from 0 used in the book. Inverse of \code{\link{NumberToSymbol}}.
+#' 
+#' @param symbol character equal to either A, C, G, or T
+#' @examples
+#' SymbolToNumber("A")
+SymbolToNumber <- function(symbol){
+	nucleotides <- c("A", "C", "G", "T")
+	if(symbol %in% nucleotides){
+		# book uses indexing starting at 0
+		number <- (which(symbol == nucleotides) - 1)
+		return(number)
+	} else {
+		return(NA)
+	}
+
+}
+
+#' Transforms nucleotide sequence into integer
+#' 
+#' \code{PatternToNumber} transforms a character string consisting of the symbols A, C, G, and T (eg a DNA sequence) into an integer representing its lexicographic order. Note that the indexing starts at 0 to correspond with the indexing used in the book. Inverse function of \code{\link{NumberToPattern}}.
+#' 
+#' @param pattern character string consisting only of the characters A, C, G, or T
+#' @examples
+#' PatternToNumber("AGT")
+PatternToNumber <- function(pattern){
+	nucleotides <- c("A", "C", "G", "T")
+	#check if pattern is empty
+	if(identical(pattern, "")){
+		return(0)
+	}
+	symbol <- substr(pattern, nchar(pattern), nchar(pattern))
+	prefix <- substr(pattern, 1, nchar(pattern) - 1)
+	return(4 * PatternToNumber(prefix) + SymbolToNumber(symbol))
+}
+
+#' Transforms lexicographic index into corresponding nucleotide symbol
+#' 
+#' \code{NumberToSymbol} transforms the integers 0, 1, 2, and 3 into the symbols A, C, G, and T with the corresponding lexicographic index. This corresponds to the indexing starting from 0 used in the book. Inverse of \code{\link{SymbolToNumber}}.
+#' 
+#' @param index integer giving the lexicographic index of the symbol to be returned
+#' @examples
+#' NumberToSymbol(0)
+NumberToSymbol <- function(index){
+	nucleotides <- c("A", "C", "G", "T")
+	# book uses indexing starting at 0
+	return(nucleotides[index + 1])
+}
+
+#' Converts integer index to corresponding nucleotide sequence
+#' 
+#' \code{NumberToPattern} takes an integer \code{index} and converts it to the nucleotide of length \code{k} that has that index among \emph{k}-mers arranged by lexicographic order. Note that indexing starts at 0 to correspond with the indexing convention used in the book. Inverse function of \code{\link{PatternToNumber}}.
+#' 
+#' @param index integer which give the lexicographic order of the \emph{k}-mer pattern to be found
+#' @param k integer which gives length of nucleotide sequence
+#' @examples
+#' NumberToPattern(11, 3)
+NumberToPattern <- function(index, k){
+	if(identical(k,1)){
+		return(NumberToSymbol(index))
+	}
+	prefix_index <- index %/% 4
+	r <- index %% 4
+	symbol <- NumberToSymbol(r)
+	prefix_pattern <- NumberToPattern(prefix_index, k - 1)
+	return(paste0(prefix_pattern, symbol))
 }
