db53db76e492cc8fc955d46b3754206c00b14073
Shannon Largman <shannon.largman@gmail.com>
2019-10-30 15:27:34 -0700

    Add script for solving all Ch. 1 problems on Rosalind
    Completes ch1_script.R which contains the code used to solve all the
    Code Challenges for Ch. 1 on Rosalind.

--- scripts/ch1_script.R
+++ scripts/ch1_script.R
@@ -21,6 +21,95 @@ occurences <- PatternMatch(pattern, genome)
 output <- paste(occurences, collapse = " ")
 writeClipboard(output)
 
-writeClipboard(paste0(as.character(PatternMatch(readLines("dataset_3_5.txt")[[1]], readLines("dataset_3_5.txt")[[2]])), collapse = " "))
+# 1E
+dataset <- readLines("../data/rosalind_ba1e.txt")
+genome <- dataset[[1]]
+vars <- as.integer(strsplit(dataset[[2]], split = " ")[[1]])
+k <- vars[1]
+L <- vars[2]
+t <- vars[3]
+clumps <- ClumpFinding(genome, k, t, L)
+output <- paste(clumps, collapse = " ")
+writeClipboard(output)
+
+
+# 1F
+dataset <- readLines("../data/rosalind_ba1f.txt")
+genome <- dataset[[1]]
+answer <- FindMinimumSkew(genome)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1G
+dataset <- readLines("../data/rosalind_ba1g.txt")
+s1 <- dataset[[1]]
+s2 <- dataset[[2]]
+answer <- HammingDistance(s1, s2)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+
+# 1H
+dataset <- readLines("../data/rosalind_ba1h.txt")
+#dataset <- list("ATTCTGGA", "CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAATGCCTAGCGGCTTGTGGTTTCTCCTACGCTCC", 3)
+pattern <- dataset[[1]]
+text <- dataset[[2]]
+d <- as.integer(dataset[[3]])
+answer <- ApproximatePatternMatch(pattern, text, d)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1I
+dataset <- readLines("../data/rosalind_ba1i.txt")
+text <- dataset[[1]]
+vars <- as.integer(strsplit(dataset[[2]], split = " ")[[1]])
+k <- vars[1]
+d <- vars[2]
+answer <- FrequentWordsWithMismatches(text, k, d)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1J
+dataset <- readLines("../data/rosalind_ba1j.txt")
+#dataset <- list("ACGTTGCATGTCGCATGATGCATGAGAGCT", "4 1")
+text <- dataset[[1]]
+vars <- as.integer(strsplit(dataset[[2]], split = " ")[[1]])
+k <- vars[1]
+d <- vars[2]
+answer <- FrequentWordsWithMismatchesAndComplements(text, k, d)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1K
+dataset <- readLines("../data/rosalind_ba1k.txt")
+text <- dataset[[1]]
+k <- as.integer(dataset[[2]])
+answer <- ComputingFrequencies(text, k)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1L
+dataset <- readLines("../data/rosalind_ba1l.txt")
+#dataset <- "AGT"
+pattern <- dataset[[1]]
+answer <- PatternToNumber(pattern)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1M
+dataset <- readLines("../data/rosalind_ba1m.txt")
+index <- as.integer(dataset[[1]])
+k <- as.integer(dataset[[2]])
+answer <- NumberToPattern(index, k)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 1N
+dataset <- readLines("../data/rosalind_ba1n.txt")
+pattern <- dataset[[1]]
+d <- as.integer(dataset[[2]])
+answer <- Neighbors(pattern, d)
+output <- paste(answer, collapse = "\n")
+writeClipboard(output)
 
 cholerae <- readLines("vibrio_cholerae.txt")
