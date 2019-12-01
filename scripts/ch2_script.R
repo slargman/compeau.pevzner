0bec3d0bc7a7a007e6656f41b6828c2b5be66f6c
Shannon Largman <shannon.largman@gmail.com>
2019-12-01 00:25:08 -0800

    Add scripts for 2B and 2C on Rosalind
    The given code for 2B and 2C was sucessfully used to solve Code
    Challenge 2B (Median String) and 2C (Profile-most Probable k-mer).

--- scripts/ch2_script.R
+++ scripts/ch2_script.R
@@ -1,8 +1,29 @@
-
+# 2A
 dataset <- readLines("../data/rosalind_ba2a.txt")
 k <- as.integer(substring(dataset[1], 1, 1)[[1]])
 d <- as.integer(substring(dataset[1], 3, 3)[[1]])
 dna <- dataset[-1]
 answer <- MotifEnumeration(dna, k, d)
+output <- paste(answer, collapse = " ")
+writeClipboard(output)
+
+# 2B
+dataset <- readLines("../data/rosalind_ba2b.txt")
+k <- as.integer(dataset[1])
+dna <- dataset[-1]
+answer <- MedianString(dna, k)
+output <- answer
+writeClipboard(output)
+
+# 2C
+dataset <- readLines("../data/rosalind_ba2c.txt")
+text <- dataset[1]
+k <- as.integer(dataset[2])
+A <- as.numeric(strsplit(dataset[3], split = " ")[[1]])
+C <- as.numeric(strsplit(dataset[4], split = " ")[[1]])
+G <- as.numeric(strsplit(dataset[5], split = " ")[[1]])
+T <- as.numeric(strsplit(dataset[6], split = " ")[[1]])
+profile <- rbind(A, C, G, T)
+answer <- ProfileMostProbableString(text, k, profile)
 output <- paste(answer, collapse = " ")
 writeClipboard(output)
