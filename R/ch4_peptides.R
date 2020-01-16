705a132ddb253f814c4c938439f63c1e359fe7e8
Shannon Largman <shannon.largman@gmail.com>
2020-01-16 14:00:41 -0800

    Generate genetic code vector of codons and amino acids

---
+++ R/ch4_peptides.R
@@ -0,0 +1,9 @@
+RNA_nuc <- c("A", "C", "G", "U")
+
+# generate codons
+codons <- expand.grid(rep(list(RNA_nuc), 3), stringsAsFactors = FALSE)
+codons <- do.call(paste0, as.list(codons[, 3:1]))
+
+# amino acids
+genetic_code <- c("K", "N", "K", "N", "T", "T", "T", "T", "R", "S", "R", "S", "I", "I", "M", "I", "Q", "H", "Q", "H", "P", "P", "P", "P", "R", "R", "R", "R", "L", "L", "L", "L", "E", "D", "E", "D", "A", "A", "A", "A", "G", "G", "G", "G", "V", "V", "V", "V", "*", "Y", "*", "Y", "S", "S", "S", "S", "*", "C", "W", "C", "L", "F", "L", "F") 
+names(genetic_code) <- codons
