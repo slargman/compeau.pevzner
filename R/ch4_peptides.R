6152f11a9dce874637ffe70ad7e32d0e4b2d1c54
Shannon Largman <shannon.largman@gmail.com>
2020-01-16 14:15:39 -0800

    Add function to translate RNA into a peptide (4A)

--- R/ch4_peptides.R
+++ R/ch4_peptides.R
@@ -7,3 +7,18 @@ codons <- do.call(paste0, as.list(codons[, 3:1]))
 # amino acids
 genetic_code <- c("K", "N", "K", "N", "T", "T", "T", "T", "R", "S", "R", "S", "I", "I", "M", "I", "Q", "H", "Q", "H", "P", "P", "P", "P", "R", "R", "R", "R", "L", "L", "L", "L", "E", "D", "E", "D", "A", "A", "A", "A", "G", "G", "G", "G", "V", "V", "V", "V", "*", "Y", "*", "Y", "S", "S", "S", "S", "*", "C", "W", "C", "L", "F", "L", "F") 
 names(genetic_code) <- codons
+TranslateCodon <- function(codon){
+	translated_codon <- genetic_code[codon]
+	names(translated_codon) <- NULL
+	return(translated_codon)
+}
+
+TranslateRNA <- function(RNA){
+	codons <- substring(RNA, seq(1, nchar(RNA), 3), seq(3, nchar(RNA), 3))
+	peptide <- paste(TranslateCodon(codons), sep = "", collapse = "")
+	n <- nchar(peptide)
+	if (identical("*", substring(peptide, n))) {
+		peptide <- substring(peptide, 1, n - 1)
+	}
+	return(peptide)
+}
